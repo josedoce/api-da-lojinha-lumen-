b03513501c5bb81cb37142a36d53ee9c22646f89
josedoce <josesamya@gmail.com>
2021-04-11 00:24:52 -0300

    primeiro:database

---
+++ database/migrations/2021_04_09_213715_create_usuario.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsuario extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('usuario', function (Blueprint $table) {
+            $table->id('uuid');
+            $table->string('nome');
+            $table->integer('idade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('usuario');
+    }
+}
