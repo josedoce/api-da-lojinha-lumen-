036b97f531b23d1fecc1cbab5f666fa063e29a60
josedoce <josesamya@gmail.com>
2021-04-11 00:26:35 -0300

    primeiro:config de projeto

---
+++ server_start.sh
@@ -0,0 +1,11 @@
+echo =============blogzinho=============
+echo Acesse: http://localhost:8000
+echo ===================================
+echo Autor: https://github.com/josedoce
+echo ===================================
+echo Obs: remova este script em prod \:\)
+echo ===================================
+
+
+php -S localhost:8000 -t public
+echo "";
