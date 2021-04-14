ba061e1346eb5b69ffa0e3235a74084b78f961cc
josedoce <josesamya@gmail.com>
2021-04-14 19:06:47 -0300

    segundo: adição do controller numero

---
+++ app/Http/Controllers/NumeroController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Services\NumeroService;
+use Illuminate\Http\Request;
+
+class NumeroController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    private $numero;
+    public function __construct(NumeroService $numero)
+    {
+        $this->numero = $numero;
+    }
+
+    public function index()
+    {
+        return $this->numero->getAll();
+    }
+
+    public function show($id)
+    {
+        return $this->numero->get($id);
+    }
+
+    public function store(Request $request)
+    {
+        return $this->numero->store($request);
+    }
+
+    public function update($id, Request $request)
+    {
+        return $this->numero->update($id, $request);
+    }
+
+    public function destroy($id)
+    {
+        return $this->numero->destroy($id);
+    }
+
+}
